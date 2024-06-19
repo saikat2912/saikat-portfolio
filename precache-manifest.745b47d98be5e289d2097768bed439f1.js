cc98ec942897055a1a033b3fdd50652fd0b9a5bb
saikat2912 <saikat2270@gmail.com>
2024-06-19 09:46:51 +0530

    Updates

---
+++ precache-manifest.745b47d98be5e289d2097768bed439f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e583e5aab55e41d3b6ba427543f42e76",
+    "url": "/saikat-portfolio/index.html"
+  },
+  {
+    "revision": "befa12d4ff6be4fba955",
+    "url": "/saikat-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "f11b59de6b606e597235",
+    "url": "/saikat-portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "befa12d4ff6be4fba955",
+    "url": "/saikat-portfolio/static/js/2.680b5717.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/saikat-portfolio/static/js/2.680b5717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f11b59de6b606e597235",
+    "url": "/saikat-portfolio/static/js/main.92b0126c.chunk.js"
+  },
+  {
+    "revision": "63ad4927e63576d81397",
+    "url": "/saikat-portfolio/static/js/runtime-main.ad201220.js"
+  }
+]);
